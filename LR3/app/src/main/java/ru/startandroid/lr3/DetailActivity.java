6f24044ec944d817ef180f3f5aaaa3b01a0a2a57
PolinaPPS <polski7595@gmail.com>
2020-12-09 13:52:15 +0700

    Создала Intent, передала его на другую активность. Исправила ошибки.

--- LR3/app/src/main/java/ru/startandroid/lr3/DetailActivity.java
+++ LR3/app/src/main/java/ru/startandroid/lr3/DetailActivity.java
@@ -3,6 +3,7 @@ package ru.startandroid.lr3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Bundle arguments = getIntent().getExtras();
+        String detail = arguments.get("ARG_ITEM_ID").toString();
+        TextView details = findViewById(R.id.textView_details);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Информация об элементе: ").append(detail);
+        for (int i=0; i < Integer.parseInt(detail);i++)
+        {
+            sb.append("\n Детальная инормация. ");
+        }
+        details.setText(sb);
     }
 }
--- LR3/app/src/main/java/ru/startandroid/lr3/MainActivity.java
+++ LR3/app/src/main/java/ru/startandroid/lr3/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.startandroid.lr3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rv = findViewById(R.id.list);
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        rv.setLayoutManager(lm);
+        RViewAdapter adapter = new RViewAdapter(DummyContent.ITEMS);
+        rv.setAdapter(adapter);
     }
 }
