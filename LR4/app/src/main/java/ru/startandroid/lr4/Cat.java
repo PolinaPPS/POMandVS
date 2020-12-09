4f496c4f232616a7c42df3e031ed5f10d94aec76
PolinaPPS <polski7595@gmail.com>
2020-12-09 15:19:41 +0700

    Создала новый класс Cat и выолнила конвертацию экземпляра класса в JSON, а затем обратно с другими свойствами объекта

---
+++ LR4/app/src/main/java/ru/startandroid/lr4/Cat.java
@@ -0,0 +1,11 @@
+package ru.startandroid.lr4;
+
+public class Cat {
+    public String name;
+    public int age;
+    public int color;
+
+    public Cat() {
+
+    }
+}
--- LR4/app/src/main/java/ru/startandroid/lr4/MainActivity.java
+++ LR4/app/src/main/java/ru/startandroid/lr4/MainActivity.java
@@ -2,7 +2,13 @@ package ru.startandroid.lr4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.BLACK;
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        TextView tx = findViewById(R.id.textView);
+        tx.setText(gson.toJson(murzik));
+
+        String jsonText = "{\"name\":\"Барсик\",\"color\":-16777216,\"age\":7}";
+        Cat barsik = gson.fromJson(jsonText, Cat.class);
+        tx.setText("Имя: " + barsik.name + "\nВозраст: " + barsik.age);
     }
 }
