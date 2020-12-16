b4019723032baf0cc941831db2b483aa77b656ae
PolinaPPS <polski7595@gmail.com>
2020-12-16 20:06:58 +0700

    Создала папку api, добавила в нее интерфейс FlickrAPI и класс ServiceAPI

---
+++ LR6/app/src/main/java/ru/startandroid/myapplication/api/FlickrAPI.java
@@ -0,0 +1,14 @@
+package ru.startandroid.myapplication.api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import ru.startandroid.myapplication.model.Response;
+
+public interface FlickrAPI {
+    @GET("services/rest/?method=flickr.photos.getRecent&api_key=eb33ee1959aca46f8dbc2fa8a56f9f24&extras=url_s&per_page=10&page=1&format=json&nojsoncallback=1")
+    Call<Response> getRecent();
+    @GET("services/rest/?method=flickr.photos.search&api_key=eb33ee1959aca46f8dbc2fa8a56f9f24&extras=url_s&per_page=10&page=1&format=json&nojsoncallback=1")
+    Call<Response> getSearchPhotos(@Query("text") String keyWord);
+
+}
---
+++ LR6/app/src/main/java/ru/startandroid/myapplication/api/ServiceAPI.java
@@ -0,0 +1,28 @@
+package ru.startandroid.myapplication.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ServiceAPI {
+    private static final String BASE_URL = "https://flickr.com/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+            return retrofit;
+        }
+        return retrofit;
+
+    }
+}
